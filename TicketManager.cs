b239c42248aa3c3dcc80a58c82140d809fbbd97f
mwayer-code <mwayer@my.wctc.edu>
2024-03-23 21:01:41 -0500

    [1.11] Update TicketManager.cs to update the streamreader to correspond to only read the file if its array length matches the length it should be for its corresponding class

--- TicketManager.cs
+++ TicketManager.cs
@@ -9,9 +9,19 @@ public class TicketManager {
         if (File.Exists(file)) {
             StreamReader sr = new StreamReader(file);
             while (!sr.EndOfStream) {
-                string ?line = sr.ReadLine();
+                string line = sr.ReadLine();
                 string[] arr = line.Split(',');
-                tickets.Add(new Ticket(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]));
+                Ticket ticket;
+                if (arr.Length == 8) {
+                    ticket = new BugDefect(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]);
+                } else if (arr.Length == 11) {
+                    ticket = new Enhancement(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], double.Parse(arr[8]), arr[9], double.Parse(arr[10]));
+                } else if (arr.Length == 9){
+                    ticket = new Task(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]);
+                } else {
+                    throw new Exception("Invalid line");
+                }
+                tickets.Add(ticket);
             }
             sr.Close();
         }

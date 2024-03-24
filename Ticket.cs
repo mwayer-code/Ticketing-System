8f6cce42394805a78d4bbe167c2dfc701fd09bca
mwayer-code <mwayer@my.wctc.edu>
2024-03-23 20:56:35 -0500

    [1.10] Update Ticket.cs to have Bug/defect enhancement and task classes to extend ticket class

--- Ticket.cs
+++ Ticket.cs
@@ -1,4 +1,4 @@
-public class Ticket {
+public abstract class Ticket {
     public string TicketID { get; set; }
     public string Summary { get; set; }
     public string Status { get; set; }
@@ -15,8 +15,44 @@ public class Ticket {
         Assigned = assigned;
         Watching = watching;
     }
-    public override string ToString() {
-        return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}";
-    }
+    public abstract override string ToString();
 
+}
+
+public class BugDefect : Ticket {
+    public string Severity { get; set; }
+    public BugDefect(string ticketID, string summary, string status, string priority, string submitter, string assigned, string watching, string severity) : base(ticketID, summary, status, priority, submitter, assigned, watching) {
+        Severity = severity;
+    }
+    public override string ToString() {
+        return TicketID + "," + Summary + "," + Status + "," + Priority + "," + Submitter + "," + Assigned + "," + Watching + "," + Severity;
+    }
+}
+
+public class Enhancement : Ticket {
+    public string Software { get; set; }
+    public double Cost { get; set; }
+    public string Reason { get; set; }
+    public double Estimate { get; set; }
+    public Enhancement(string ticketID, string summary, string status, string priority, string submitter, string assigned, string watching, string software, double cost, string reason, double estimate) : base(ticketID, summary, status, priority, submitter, assigned, watching) {
+        Software = software;
+        Cost = cost;
+        Reason = reason;
+        Estimate = estimate;
+    }
+    public override string ToString() {
+        return TicketID + "," + Summary + "," + Status + "," + Priority + "," + Submitter + "," + Assigned + "," + Watching + "," + Software + "," + Cost + "," + Reason + "," + Estimate;
+    }
+}
+
+public class Task : Ticket {
+    public string ProjectName { get; set; }
+    public string DueDate { get; set; }
+    public Task(string ticketID, string summary, string status, string priority, string submitter, string assigned, string watching, string projectName, string dueDate) : base(ticketID, summary, status, priority, submitter, assigned, watching) {
+        ProjectName = projectName;
+        DueDate = dueDate;
+    }
+    public override string ToString() {
+        return TicketID + "," + Summary + "," + Status + "," + Priority + "," + Submitter + "," + Assigned + "," + Watching + "," + ProjectName + "," + DueDate;
+    }
 }
